1028af6165e6e805f7a74578cde692c71f1afc32
Your Name <you@example.com>
2021-06-04 11:31:13 +0200

    add ci

--- installtoolchain.sh
+++ installtoolchain.sh
@@ -9,7 +9,7 @@ then
     mkdir ~/toolchain && cd ~/toolchain
     wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
     tar -xvjf gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
-    echo "export PATH=$(pwd)/gcc-arm-none-eabi-9-2020-q2-update:$PATH" > ~/.bashrc
+    echo "export PATH=$(pwd)/gcc-arm-none-eabi-9-2020-q2-update:$PATH" >> ~/.bashrc
 else
     echo "Already installed ARM toolchain"
 fi
