b4e0061e903ca7e61ccc31e7d2adccb890188c62
Your Name <you@example.com>
2021-06-14 10:50:31 +0200

    updates

--- installtoolchain.sh
+++ installtoolchain.sh
@@ -5,8 +5,8 @@ if [ ! -d ~/toolchain ]
 then
     mkdir ~/toolchain && cd ~/toolchain
     # wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
-    wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-aarch64-linux.tar.bz2
     # tar -xvjf gcc-arm-none-eabi-9-2020-q2-update-x86_64-linux.tar.bz2
+    wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-aarch64-linux.tar.bz2
     tar -xvjf gcc-arm-none-eabi-9-2020-q2-update-aarch64-linux.tar.bz2
     echo "export PATH=~/toolchain/gcc-arm-none-eabi-9-2020-q2-update/bin:$PATH" >> ~/.bashrc
 else
