6b30b84f068aa70c6ac65a6f6df1998fa3380d39
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2021-06-16 11:49:04 +0200

    Set relative paths on project build (#5)

--- e2studio_project_threadX/micro-ROS_tests/src/subdir.mk
+++ e2studio_project_threadX/micro-ROS_tests/src/subdir.mk
@@ -6,6 +6,7 @@
 C_SRCS += \
 ../src/hal_entry.c \
 ../src/microros_allocators.c \
+../src/microros_app.c \
 ../src/microros_time.c \
 ../src/thread_microros_entry.c \
 ../src/udp_transport.c 
@@ -13,6 +14,7 @@ C_SRCS += \
 OBJS += \
 ./src/hal_entry.o \
 ./src/microros_allocators.o \
+./src/microros_app.o \
 ./src/microros_time.o \
 ./src/thread_microros_entry.o \
 ./src/udp_transport.o 
@@ -20,6 +22,7 @@ OBJS += \
 C_DEPS += \
 ./src/hal_entry.d \
 ./src/microros_allocators.d \
+./src/microros_app.d \
 ./src/microros_time.d \
 ./src/thread_microros_entry.d \
 ./src/udp_transport.d 
@@ -29,7 +32,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM Cross C Compiler'
-	arm-none-eabi-gcc -mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -D_RENESAS_RA_ -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/src" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/micro_ros_renesas2estudio_component/libmicroros/include" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/fsp/inc" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/fsp/inc/api" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/fsp/inc/instances" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/arm/CMSIS_5/CMSIS/Core/Include" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/fsp/src/rm_threadx_port" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/threadx/common/inc" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra_gen" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra_cfg/fsp_cfg/bsp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra_cfg/fsp_cfg" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra_cfg/fsp_cfg/azure/tx" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/pop3" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/snmp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/nat" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/mqtt" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/ftp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/tftp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/sntp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/smtp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/web" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/auto_ip" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/telnet" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/dns" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/dhcp" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/addons/http" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/common/inc" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/microsoft/azure-rtos/netxduo/ports/cortex_m4" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra/fsp/src/rm_netxduo_ether" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra_cfg/fsp_cfg/middleware" -I"/home/micro_ros_renesas_testbench/e2studio_project_threadX/ra_cfg/fsp_cfg/azure/nxd" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" -x c "$<"
+	arm-none-eabi-gcc -mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -D_RENESAS_RA_ -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../ra/fsp/inc/instances" -I"../ra/arm/CMSIS_5/CMSIS/Core/Include" -I"../ra/fsp/src/rm_threadx_port" -I"../ra/microsoft/azure-rtos/threadx/common/inc" -I"../ra_gen" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_cfg/fsp_cfg" -I"../ra_cfg/fsp_cfg/azure/tx" -I"../ra/microsoft/azure-rtos/netxduo/addons/pop3" -I"../ra/microsoft/azure-rtos/netxduo/addons/snmp" -I"../ra/microsoft/azure-rtos/netxduo/addons/nat" -I"../ra/microsoft/azure-rtos/netxduo/addons/mqtt" -I"../ra/microsoft/azure-rtos/netxduo/addons/ftp" -I"../ra/microsoft/azure-rtos/netxduo/addons/tftp" -I"../ra/microsoft/azure-rtos/netxduo/addons/sntp" -I"../ra/microsoft/azure-rtos/netxduo/addons/smtp" -I"../ra/microsoft/azure-rtos/netxduo/addons/web" -I"../ra/microsoft/azure-rtos/netxduo/addons/auto_ip" -I"../ra/microsoft/azure-rtos/netxduo/addons/telnet" -I"../ra/microsoft/azure-rtos/netxduo/addons/dns" -I"../ra/microsoft/azure-rtos/netxduo/addons/dhcp" -I"../ra/microsoft/azure-rtos/netxduo/addons/http" -I"../ra/microsoft/azure-rtos/netxduo/common/inc" -I"../ra/microsoft/azure-rtos/netxduo/ports/cortex_m4" -I"../ra/fsp/src/rm_netxduo_ether" -I"../ra_cfg/fsp_cfg/middleware" -I"../ra_cfg/fsp_cfg/azure/nxd" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" -x c "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
