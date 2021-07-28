deb56a0ce1aae1106317da85c5dadda18cb52add
Pablo Garrido <pablogs9@gmail.com>
2021-07-28 14:01:37 +0200

    initial (#17)
    initial
    
    Enable tracefacilities
    
    
    Update
    
    
    update
    
    Revert "initial"
    
    This reverts commit c656d3fe5e9c86fcd119544693f079bef52c8d2b.
    
    Initial profiling

--- e2studio_project_freeRTOS/src/net_thread_entry.c
+++ e2studio_project_freeRTOS/src/net_thread_entry.c
@@ -4,6 +4,7 @@
 #include "FreeRTOS_Sockets.h"
 
 #include "config.h"
+#include "./utils.h"
 
 #include <microros_transports.h>
 #include <microros_allocators.h>
@@ -16,6 +17,63 @@
 #include <rmw_microros/rmw_microros.h>
 
 void microros_app(void);
+
+#ifdef MICROROS_PROFILING
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+#include <std_msgs/msg/u_int32_multi_array.h>
+
+#define RCCHECK(fn, led) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){bool a = 1;while(1){set_led_status(led, a = !a); sleep_ms(100);}}}
+
+extern size_t measured_max_used_stack;
+extern size_t measured_absoluteUsedMemory;
+extern size_t measured_usedMemory;
+
+void * microros_allocate_stub(size_t size, void * state);
+void microros_deallocate_stub(void * pointer, void * state);
+void * microros_reallocate_stub(void * pointer, size_t size, void * state);
+void * microros_zero_allocate_stub(size_t number_of_elements, size_t size_of_element, void * state);
+size_t get_block_data_size(void * pointer);
+void send_micro_ros_profiling_data(void);
+
+size_t absoluteUsedMemory = 0;
+size_t usedMemory = 0;
+
+size_t get_block_data_size(void * pointer){
+    // Assuming size_t length at the end of the header struct
+    size_t *puc = (size_t *) pointer;
+    puc--;
+    return *puc;
+}
+
+void * microros_allocate_stub(size_t size, void * state){
+    void * ptr = microros_allocate(size, state);
+    absoluteUsedMemory += get_block_data_size(ptr);
+    usedMemory += get_block_data_size(ptr);
+    return ptr;
+}
+
+void microros_deallocate_stub(void * pointer, void * state){
+    usedMemory -= get_block_data_size(pointer);
+    return microros_deallocate(pointer, state);
+}
+
+void * microros_reallocate_stub(void * pointer, size_t size, void * state){
+    usedMemory -= get_block_data_size(pointer);
+    void * ptr = microros_reallocate(pointer, size, state);
+    usedMemory += get_block_data_size(ptr);
+    absoluteUsedMemory += get_block_data_size(ptr);
+    return ptr;
+}
+
+void * microros_zero_allocate_stub(size_t number_of_elements, size_t size_of_element, void * state){
+    void * ptr = microros_zero_allocate(number_of_elements, size_of_element, state);
+    absoluteUsedMemory += get_block_data_size(ptr);
+    usedMemory += get_block_data_size(ptr);
+    return ptr;
+}
+#endif
 
 void net_thread_entry(void *pvParameters)
 {
@@ -36,14 +94,66 @@ void net_thread_entry(void *pvParameters)
         );
 
     rcl_allocator_t custom_allocator = rcutils_get_zero_initialized_allocator();
+
+#ifdef MICROROS_PROFILING
+    custom_allocator.allocate = microros_allocate_stub;
+    custom_allocator.deallocate = microros_deallocate_stub;
+    custom_allocator.reallocate = microros_reallocate_stub;
+    custom_allocator.zero_allocate =  microros_zero_allocate_stub;
+#else
     custom_allocator.allocate = microros_allocate;
     custom_allocator.deallocate = microros_deallocate;
     custom_allocator.reallocate = microros_reallocate;
     custom_allocator.zero_allocate =  microros_zero_allocate;
+#endif
 
     if (!rcutils_set_default_allocator(&custom_allocator)) {
         printf("Error on default allocators (line %d)\n", __LINE__);
     }
 
     microros_app();
+
+#ifdef MICROROS_PROFILING
+    send_micro_ros_profiling_data();
+#endif
 }
+
+#ifdef MICROROS_PROFILING
+void send_micro_ros_profiling_data(void)
+{
+    rcl_allocator_t allocator = rcl_get_default_allocator();
+
+    //create init_options
+    rclc_support_t support;
+    rclc_support_init(&support, 0, NULL, &allocator);
+
+    // create nodes
+	rcl_node_t node;
+	rcl_node_options_t node_ops = rcl_node_get_default_options();
+	node_ops.domain_id = (size_t)(DOMAIN_ID);
+	rclc_node_init_with_options(&node, "profiling_node", "", &support, &node_ops);
+
+    // create publisher
+    rcl_publisher_t publisher;
+    rclc_publisher_init_default(
+        &publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, UInt32MultiArray),
+        "profiling_output");
+
+    std_msgs__msg__UInt32MultiArray msg;
+    // Fill header
+    msg.layout = (std_msgs__msg__MultiArrayLayout) {0};
+
+    // Fill data
+    uint32_t data[] = {measured_max_used_stack, measured_absoluteUsedMemory, measured_usedMemory};
+    msg.data.data = data;
+    msg.data.size = sizeof(data)/sizeof(data[0]);
+    msg.data.capacity = msg.data.size;
+
+    while(1){
+        rcl_publish(&publisher, &msg, NULL);
+        sleep_ms(500);
+    }
+}
+#endif
