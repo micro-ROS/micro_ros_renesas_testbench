7dcf53b10f57d8df2ef9360c355d3cd586235251
Pablo Garrido <pablogs9@gmail.com>
2021-09-17 09:34:35 +0200

    Serial

--- e2studio_project_serial/ra/fsp/src/bsp/mcu/all/bsp_arm_exceptions.h
+++ e2studio_project_serial/ra/fsp/src/bsp/mcu/all/bsp_arm_exceptions.h
@@ -18,41 +18,33 @@
  * OF SUCH LOSS, DAMAGES, CLAIMS OR COSTS.
  **********************************************************************************************************************/
 
-#ifndef SYSTEM_RENESAS_ARM_H
- #define SYSTEM_RENESAS_ARM_H
+/** @} (end addtogroup BSP_MCU) */
 
- #ifdef __cplusplus
-extern "C" {
- #endif
+#ifndef BSP_ARM_EXCEPTIONS_H
+#define BSP_ARM_EXCEPTIONS_H
 
- #include <stdint.h>
+/***********************************************************************************************************************
+ * Macro definitions
+ **********************************************************************************************************************/
 
-extern uint32_t SystemCoreClock;       /** System Clock Frequency (Core Clock)  */
+/***********************************************************************************************************************
+ * Typedef definitions
+ **********************************************************************************************************************/
 
-/**
- * Initialize the system
- *
- * @param  none
- * @return none
- *
- * @brief  Setup the microcontroller system.
- *         Initialize the System and update the SystemCoreClock variable.
- */
-extern void SystemInit(void);
-
-/**
- * Update SystemCoreClock variable
- *
- * @param  none
- * @return none
- *
- * @brief  Updates the SystemCoreClock with current core Clock
- *         retrieved from cpu registers.
- */
-extern void SystemCoreClockUpdate(void);
-
- #ifdef __cplusplus
-}
- #endif
+/* This list includes only Arm standard exceptions. Renesas interrupts are defined in vector_data.h. */
+typedef enum IRQn
+{
+    Reset_IRQn            = -15,       /*  1 Reset Vector invoked on Power up and warm reset */
+    NonMaskableInt_IRQn   = -14,       /*  2 Non maskable Interrupt cannot be stopped or preempted */
+    HardFault_IRQn        = -13,       /*  3 Hard Fault all classes of Fault */
+    MemoryManagement_IRQn = -12,       /*  4 Memory Management MPU mismatch, including Access Violation and No Match */
+    BusFault_IRQn         = -11,       /*  5 Bus Fault Pre-Fetch-, Memory Access, other address/memory Fault */
+    UsageFault_IRQn       = -10,       /*  6 Usage Fault i.e. Undef Instruction, Illegal State Transition */
+    SecureFault_IRQn      = -9,        /*  7 Secure Fault Interrupt */
+    SVCall_IRQn           = -5,        /* 11 System Service Call via SVC instruction */
+    DebugMonitor_IRQn     = -4,        /* 12 Debug Monitor */
+    PendSV_IRQn           = -2,        /* 14 Pendable request for system service */
+    SysTick_IRQn          = -1,        /* 15 System Tick Timer */
+} IRQn_Type;
 
 #endif
