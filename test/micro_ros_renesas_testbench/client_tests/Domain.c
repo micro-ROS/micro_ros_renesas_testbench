e92992b70802c7f395342ed0deadcf0d77e230e0
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2021-11-03 10:45:34 +0100

    Update domain_id API (#32)
    * Update domain id API
    
    * Convert indentation to spaces
    
    Co-authored-by: Pablo Garrido <pablogs9@gmail.com>

--- test/micro_ros_renesas_testbench/client_tests/Domain.c
+++ test/micro_ros_renesas_testbench/client_tests/Domain.c
@@ -23,11 +23,11 @@ std_msgs__msg__Int32 msg;
 
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time)
 {
-	(void) last_call_time;
-	if (timer != NULL) {
-		rcl_publish(&publisher, &msg, NULL);
-		msg.data++;
-	}
+    (void) last_call_time;
+    if (timer != NULL) {
+        rcl_publish(&publisher, &msg, NULL);
+        msg.data++;
+    }
 }
 
 void microros_app(void)
@@ -36,38 +36,40 @@ void microros_app(void)
 
     //create init_options
     rclc_support_t support;
-    rclc_support_init(&support, 0, NULL, &allocator);
 
-	// create node
-	rcl_node_t node;
-	rcl_node_options_t node_ops = rcl_node_get_default_options();
-	node_ops.domain_id = (size_t)(DOMAIN_ID);
-	rclc_node_init_with_options(&node, "test_node", "", &support, &node_ops);
+    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    rcl_init_options_init(&init_options, allocator);
+    rcl_init_options_set_domain_id(&init_options, DOMAIN_ID);
+    rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator);
 
-	// create publisher
-	rclc_publisher_init_default(
-		&publisher,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"test_publisher_domain");
+    // create node
+    rcl_node_t node;
+    rclc_node_init_default(&node, "test_node", "", &support);
 
-	// create timer,
-	rcl_timer_t timer;
-	const unsigned int timer_timeout = 1000;
-	rclc_timer_init_default(
-		&timer,
-		&support,
-		RCL_MS_TO_NS(timer_timeout),
-		timer_callback);
+    // create publisher
+    rclc_publisher_init_default(
+        &publisher,
+        &node,
+        ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+        "test_publisher_domain");
 
-	// create executor
-	rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
-	rclc_executor_init(&executor, &support.context, 1, &allocator);
-	rclc_executor_add_timer(&executor, &timer);
+    // create timer,
+    rcl_timer_t timer;
+    const unsigned int timer_timeout = 1000;
+    rclc_timer_init_default(
+        &timer,
+        &support,
+        RCL_MS_TO_NS(timer_timeout),
+        timer_callback);
 
-	msg.data = 0;
+    // create executor
+    rclc_executor_t executor = rclc_executor_get_zero_initialized_executor();
+    rclc_executor_init(&executor, &support.context, 1, &allocator);
+    rclc_executor_add_timer(&executor, &timer);
 
-  	for(;;){
+    msg.data = 0;
+
+      for(;;){
         rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
         sleep_ms(10);
     }
