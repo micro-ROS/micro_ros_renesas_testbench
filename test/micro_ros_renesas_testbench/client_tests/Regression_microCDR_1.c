572a8c40b08abe265896283cf1f16c18a1aff341
Pablo Garrido <pablogs9@gmail.com>
2021-11-19 12:12:41 +0100

    Fix indent

--- test/micro_ros_renesas_testbench/client_tests/Regression_microCDR_1.c
+++ test/micro_ros_renesas_testbench/client_tests/Regression_microCDR_1.c
@@ -17,13 +17,13 @@ void microros_app(void)
     //create init_options
     rclc_support_t support;
 
-	rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
-	rcl_init_options_init(&init_options, allocator);
-	rcl_init_options_set_domain_id(&init_options, DOMAIN_ID);
-	rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator);
+    rcl_init_options_t init_options = rcl_get_zero_initialized_init_options();
+    rcl_init_options_init(&init_options, allocator);
+    rcl_init_options_set_domain_id(&init_options, DOMAIN_ID);
+    rclc_support_init_with_options(&support, 0, NULL, &init_options, &allocator);
 
-	// create node
-	rcl_node_t node;
+    // create node
+    rcl_node_t node;
     rclc_node_init_default(&node, "test_node", "", &support);
 
     // create publisher
@@ -31,41 +31,41 @@ void microros_app(void)
     rclc_publisher_init_default(
         &publisher,
         &node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(tf2_msgs, msg, TFMessage),
+        ROSIDL_GET_MSG_TYPE_SUPPORT(tf2_msgs, msg, TFMessage),
         "test_pub");
 
     tf2_msgs__msg__TFMessage msg;
 
     size_t size = 30;
 
-	msg.transforms.data = (geometry_msgs__msg__TransformStamped*) malloc(size * sizeof(geometry_msgs__msg__TransformStamped));
-	msg.transforms.size = size;
-	msg.transforms.capacity = size;
+    msg.transforms.data = (geometry_msgs__msg__TransformStamped*) malloc(size * sizeof(geometry_msgs__msg__TransformStamped));
+    msg.transforms.size = size;
+    msg.transforms.capacity = size;
 
-	for(size_t i = 0; i < size; i++)
-	{
-		msg.transforms.data[i].header.frame_id.data = (char *) malloc(sizeof(char) * 100);
-		snprintf(msg.transforms.data[i].header.frame_id.data, 10, "frame_%ld", i);
-		msg.transforms.data[i].header.frame_id.size = strlen(msg.transforms.data[i].header.frame_id.data);
-		msg.transforms.data[i].header.frame_id.capacity = 100;
+    for(size_t i = 0; i < size; i++)
+    {
+        msg.transforms.data[i].header.frame_id.data = (char *) malloc(sizeof(char) * 100);
+        snprintf(msg.transforms.data[i].header.frame_id.data, 10, "frame_%ld", i);
+        msg.transforms.data[i].header.frame_id.size = strlen(msg.transforms.data[i].header.frame_id.data);
+        msg.transforms.data[i].header.frame_id.capacity = 100;
 
-		msg.transforms.data[i].header.stamp.sec = i;
-		msg.transforms.data[i].header.stamp.nanosec = i;
+        msg.transforms.data[i].header.stamp.sec = i;
+        msg.transforms.data[i].header.stamp.nanosec = i;
 
-		msg.transforms.data[i].child_frame_id.data = (char *) malloc(sizeof(char) * 100);
-		snprintf(msg.transforms.data[i].child_frame_id.data, 10, "child_%ld", i);
-		msg.transforms.data[i].child_frame_id.size = strlen(msg.transforms.data[i].child_frame_id.data);
-		msg.transforms.data[i].child_frame_id.capacity = 100;
+        msg.transforms.data[i].child_frame_id.data = (char *) malloc(sizeof(char) * 100);
+        snprintf(msg.transforms.data[i].child_frame_id.data, 10, "child_%ld", i);
+        msg.transforms.data[i].child_frame_id.size = strlen(msg.transforms.data[i].child_frame_id.data);
+        msg.transforms.data[i].child_frame_id.capacity = 100;
 
-		msg.transforms.data[i].transform.translation.x = i;
-		msg.transforms.data[i].transform.translation.y = i;
-		msg.transforms.data[i].transform.translation.z = i;
+        msg.transforms.data[i].transform.translation.x = i;
+        msg.transforms.data[i].transform.translation.y = i;
+        msg.transforms.data[i].transform.translation.z = i;
 
-		msg.transforms.data[i].transform.rotation.x = i;
-		msg.transforms.data[i].transform.rotation.y = i;
-		msg.transforms.data[i].transform.rotation.z = i;
-		msg.transforms.data[i].transform.rotation.w = i;
-	}
+        msg.transforms.data[i].transform.rotation.x = i;
+        msg.transforms.data[i].transform.rotation.y = i;
+        msg.transforms.data[i].transform.rotation.z = i;
+        msg.transforms.data[i].transform.rotation.w = i;
+    }
 
     for(;;){
         rcl_publish(&publisher, &msg, NULL);
