34643ba1bed56f40ad1c96816006eb014649e15c
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2021-12-22 08:56:28 +0100

    Update FSP to v3.5.0 (#76)
    * Upgrade fsp to 3.5.0
    
    * Fix threadX
    
    * Fix freeRTOS
    
    * Add compilation flags to threadX
    
    * Minor fix
    
    (cherry picked from commit b5d83fdb347ab860ed230340828633327c1b2531)

--- e2studio_project_freeRTOS/ra_gen/hal_data.h
+++ e2studio_project_freeRTOS/ra_gen/hal_data.h
@@ -4,7 +4,19 @@
 #include <stdint.h>
 #include "bsp_api.h"
 #include "common_data.h"
+#include "r_agt.h"
+#include "r_timer_api.h"
 FSP_HEADER
+/** AGT Timer Instance */
+extern const timer_instance_t g_timer0;
+
+/** Access the AGT instance using these structures when calling API functions directly (::p_api is not used). */
+extern agt_instance_ctrl_t g_timer0_ctrl;
+extern const timer_cfg_t g_timer0_cfg;
+
+#ifndef micro_ros_timer_cb
+void micro_ros_timer_cb(timer_callback_args_t *p_args);
+#endif
 void hal_entry(void);
 void g_hal_init(void);
 FSP_FOOTER
