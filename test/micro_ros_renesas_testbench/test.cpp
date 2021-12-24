866a4d41c56f7a6b944875ad98973f00d8393a01
acuadros95 <acuadros1995@gmail.com>
2021-12-24 14:08:31 +0100

    Add multiple boards testing

--- test/micro_ros_renesas_testbench/test.cpp
+++ test/micro_ros_renesas_testbench/test.cpp
@@ -25,6 +25,43 @@
 
 using namespace std::chrono_literals;
 
+const std::vector<board> testbench_boards =
+{
+  {
+    // device_name
+    "R7FA6M5BH3CFC",
+    // folder
+    "EK_RA6M5",
+    // serial_port
+    "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0",
+    // usb_port
+    "/dev/serial/by-id/usb-RENESAS_CDC_USB_Demonstration_0000000000001-if00",
+    // transports
+    {
+      TestAgent::Transport::CAN_TRANSPORT,
+      TestAgent::Transport::USB_TRANSPORT,
+      TestAgent::Transport::SERIAL_TRANSPORT,
+      TestAgent::Transport::UDP_THREADX_TRANSPORT,
+      TestAgent::Transport::UDP_FREERTOS_TRANSPORT
+    }
+  },
+  {
+    // device_name
+    "R7FA6T2BD3CFP",  // TODO: check
+    // folder
+    "MCK_RA6T2",
+    // serial_port
+    "...",
+    // usb_port
+    "...",
+    // transports
+    {
+      TestAgent::Transport::CAN_TRANSPORT,
+      TestAgent::Transport::SERIAL_TRANSPORT
+    }
+  }
+};
+
 TEST_P(HardwareTestAllTransports, EntityCreation) {
   std::vector<std::string> node_list =
   {
