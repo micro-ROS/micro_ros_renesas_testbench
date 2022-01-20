604c16fb14676e52e1179093f066d68b18669901
acuadros95 <acuadros1995@gmail.com>
2022-01-20 15:58:33 +0100

    Add wifi project

---
+++ e2studio_project_wifi/ra_gen/common_data.h
@@ -0,0 +1,46 @@
+/* generated common header file - do not edit */
+#ifndef COMMON_DATA_H_
+#define COMMON_DATA_H_
+#include <stdint.h>
+#include "bsp_api.h"
+#include "r_dtc.h"
+#include "r_transfer_api.h"
+#include "r_sci_uart.h"
+#include "r_uart_api.h"
+#include "rm_wifi_onchip_silex.h"
+#include "r_ioport.h"
+#include "bsp_pin_cfg.h"
+FSP_HEADER
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer1;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer1_ctrl;
+extern const transfer_cfg_t g_transfer1_cfg;
+/* Transfer on DTC Instance. */
+extern const transfer_instance_t g_transfer0;
+
+/** Access the DTC instance using these structures when calling API functions directly (::p_api is not used). */
+extern dtc_instance_ctrl_t g_transfer0_ctrl;
+extern const transfer_cfg_t g_transfer0_cfg;
+/** UART on SCI Instance. */
+extern const uart_instance_t g_uart0;
+
+/** Access the UART instance using these structures when calling API functions directly (::p_api is not used). */
+extern sci_uart_instance_ctrl_t g_uart0_ctrl;
+extern const uart_cfg_t g_uart0_cfg;
+extern const sci_uart_extended_cfg_t g_uart0_cfg_extend;
+
+#ifndef rm_wifi_onchip_silex_uart_callback
+void rm_wifi_onchip_silex_uart_callback(uart_callback_args_t *p_args);
+#endif
+extern const wifi_onchip_silex_cfg_t g_wifi_onchip_silex_cfg;
+extern const char *g_wifi_onchip_silex_uart_cmd_baud;
+/* IOPORT Instance */
+extern const ioport_instance_t g_ioport;
+
+/* IOPORT control structure. */
+extern ioport_instance_ctrl_t g_ioport_ctrl;
+void g_common_init(void);
+FSP_FOOTER
+#endif /* COMMON_DATA_H_ */
