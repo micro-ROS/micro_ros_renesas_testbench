effab82bdaf7beee8ee4b08c6a41ba72669253db
Your Name <you@example.com>
2022-01-21 11:49:16 +0000

    Update Board to class

--- test/micro_ros_renesas_testbench/boards.cpp
+++ test/micro_ros_renesas_testbench/boards.cpp
@@ -1,23 +1,24 @@
 #include "boards.hpp"
 
-const std::vector<board> testbench_boards =
+const std::vector<Board> testbench_boards =
 {
-  {
-    // device_name
-    "R7FA6M5BH3CFC",
-    // folder
-    "EK_RA6M5",
-    // serial_port
-    "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0",
-    // usb_port
-    "/dev/serial/by-id/usb-RENESAS_CDC_USB_Demonstration_0000000000001-if00",
-    // transports
     {
-      TestAgent::Transport::CAN_TRANSPORT,
-      TestAgent::Transport::USB_TRANSPORT,
-      TestAgent::Transport::SERIAL_TRANSPORT,
-      TestAgent::Transport::UDP_THREADX_TRANSPORT,
-      TestAgent::Transport::UDP_FREERTOS_TRANSPORT
+        // device_name
+        "R7FA6M5BH3CFC",
+        // folder
+        "EK_RA6M5",
+        // serial_port
+        "/dev/serial/by-id/usb-Prolific_Technology_Inc._USB-Serial_Controller-if00-port0",
+        // usb_port
+        "/dev/serial/by-id/usb-RENESAS_CDC_USB_Demonstration_0000000000001-if00",
+        // transports
+        {
+            TestAgent::Transport::CAN_TRANSPORT,
+            TestAgent::Transport::USB_TRANSPORT,
+            TestAgent::Transport::SERIAL_TRANSPORT,
+            TestAgent::Transport::UDP_THREADX_TRANSPORT,
+            TestAgent::Transport::UDP_FREERTOS_TRANSPORT,
+            TestAgent::Transport::TCP_FREERTOS_TRANSPORT
+        }
     }
-  },
 };
