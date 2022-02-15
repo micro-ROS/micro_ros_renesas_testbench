305fdcaea78c418f756fc999d17853b6f440a6fc
Antonio Cuadros <49162117+Acuadros95@users.noreply.github.com>
2022-02-15 16:15:41 +0100

    Add RA6T2 CAN FD test project (#106)
    * Enable CAN transport on boards.cpp
    
    * Add can fd test project
    
    * Enable CAN lib build on RA6T2
    
    * Fix makefile
    
    * Fix makefile object
    
    * Increase CAN RA6T2 stack
    
    (cherry picked from commit e63b453e545fd8aacc1cf85cfdf4bb80e9458ce3)

---
+++ boards/MCK_RA6T2/e2studio_project_CAN/micro-ROS_tests/ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.c \
+../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.c 
+
+OBJS += \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.o \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.o 
+
+C_DEPS += \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/startup.d \
+./ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/%.o: ../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU Arm Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -D_RENESAS_RA_ -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../ra/fsp/inc/instances" -I"../ra/arm/CMSIS_5/CMSIS/Core/Include" -I"../ra_gen" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_cfg/fsp_cfg" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
