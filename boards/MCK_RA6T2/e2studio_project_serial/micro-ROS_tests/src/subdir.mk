ff127535f65e8b2cfc6409b874c62095121462f7
Pablo Garrido <pablogs9@gmail.com>
2024-11-27 10:31:22 +0100

    Fix path e2studio_project_serial
    Fix path e2studio_project_serial

--- boards/MCK_RA6T2/e2studio_project_serial/micro-ROS_tests/src/subdir.mk
+++ boards/MCK_RA6T2/e2studio_project_serial/micro-ROS_tests/src/subdir.mk
@@ -2,37 +2,37 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
 ../src/hal_entry.c \
 ../src/microros_allocators.c \
 ../src/microros_app.c \
 ../src/microros_time.c \
-../src/microros_transports.c 
+../src/microros_transports.c
 
 C_DEPS += \
 ./src/hal_entry.d \
 ./src/microros_allocators.d \
 ./src/microros_app.d \
 ./src/microros_time.d \
-./src/microros_transports.d 
+./src/microros_transports.d
 
 OBJS += \
 ./src/hal_entry.o \
 ./src/microros_allocators.o \
 ./src/microros_app.o \
 ./src/microros_time.o \
-./src/microros_transports.o 
+./src/microros_transports.o
 
 SREC += \
-microros_testbench.srec 
+microros_testbench.srec
 
 MAP += \
-microros_testbench.map 
+microros_testbench.map
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.c
-	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/src/r_usb_basic/src/driver/inc" -I"../ra_cfg/fsp_cfg" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_gen" -I"../ra/arm/CMSIS_5/CMSIS/Core/Include" -I"../ra/fsp/inc/instances" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/src" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra/fsp/inc" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra/fsp/inc/api" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra/fsp/inc/instances" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra_gen" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra_cfg/fsp_cfg/bsp" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra_cfg/fsp_cfg" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/MCK_RA6T2/e2studio_project_serial/ra/arm/CMSIS_6/CMSIS/Core/Include" -I"." -std=c99 -Wall -Werror -Wno-aggregate-return -Wno-unused-result -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/src/r_usb_basic/src/driver/inc" -I"../ra_cfg/fsp_cfg" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_gen" -I"../ra/arm/CMSIS_5/CMSIS/Core/Include" -I"../ra/fsp/inc/instances" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../src" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../ra/fsp/inc/instances" -I"../ra_gen" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_cfg/fsp_cfg" -I"../ra/arm/CMSIS_6/CMSIS/Core/Include" -I"." -std=c99 -Wall -Werror -Wno-aggregate-return -Wno-unused-result -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
 	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
 
