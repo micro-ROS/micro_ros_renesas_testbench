830abc6ec92d393aa98a8511a4ec10cfe1bd4bd6
Pablo Garrido <pablogs9@gmail.com>
2024-11-27 09:51:20 +0100

    Fix path e2studio_project_USB

--- boards/EK_RA6M5/e2studio_project_USB/micro-ROS_tests/ra/fsp/src/r_agt/subdir.mk
+++ boards/EK_RA6M5/e2studio_project_USB/micro-ROS_tests/ra/fsp/src/r_agt/subdir.mk
@@ -2,25 +2,25 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
-../ra/fsp/src/r_agt/r_agt.c 
+../ra/fsp/src/r_agt/r_agt.c
 
 C_DEPS += \
-./ra/fsp/src/r_agt/r_agt.d 
+./ra/fsp/src/r_agt/r_agt.d
 
 OBJS += \
-./ra/fsp/src/r_agt/r_agt.o 
+./ra/fsp/src/r_agt/r_agt.o
 
 SREC += \
-microros_testbench.srec 
+microros_testbench.srec
 
 MAP += \
-microros_testbench.map 
+microros_testbench.map
 
 
 # Each subdirectory must supply rules for building sources it contributes
 ra/fsp/src/r_agt/%.o: ../ra/fsp/src/r_agt/%.c
-	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/src/r_usb_basic/src/driver/inc" -I"../ra_cfg/fsp_cfg" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_gen" -I"../ra/arm/CMSIS_5/CMSIS/Core/Include" -I"../ra/fsp/inc/instances" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/src" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra/fsp/inc" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra/fsp/inc/api" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra/fsp/inc/instances" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra/fsp/src/r_usb_basic/src/driver/inc" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra_gen" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra_cfg/fsp_cfg/bsp" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra_cfg/fsp_cfg" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_USB/ra/arm/CMSIS_6/CMSIS/Core/Include" -I"." -std=c99 -Wall -Werror -Wno-aggregate-return -Wno-unused-result -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/src/r_usb_basic/src/driver/inc" -I"../ra_cfg/fsp_cfg" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_gen" -I"../ra/arm/CMSIS_5/CMSIS/Core/Include" -I"../ra/fsp/inc/instances" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../src" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../ra/fsp/inc/instances" -I"../ra/fsp/src/r_usb_basic/src/driver/inc" -I"../ra_gen" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_cfg/fsp_cfg" -I"../ra/arm/CMSIS_6/CMSIS/Core/Include" -I"." -std=c99 -Wall -Werror -Wno-aggregate-return -Wno-unused-result -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
 	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
 
