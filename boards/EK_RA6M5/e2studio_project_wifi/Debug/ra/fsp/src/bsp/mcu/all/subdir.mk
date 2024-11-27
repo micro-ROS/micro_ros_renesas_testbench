4ce8265168f21a9404033b2bd9d5e19390770c28
Pablo Garrido <pablogs9@gmail.com>
2024-11-27 10:30:09 +0100

    Fix path e2studio_project_wifi
    Revert "Fix path e2studio_project_wifi"
    
    This reverts commit f5f6bae2c1a03ba2b66a0bb2f2a5001637c118fa.
    
    Fix path e2studio_project_wifi

--- boards/EK_RA6M5/e2studio_project_wifi/Debug/ra/fsp/src/bsp/mcu/all/subdir.mk
+++ boards/EK_RA6M5/e2studio_project_wifi/Debug/ra/fsp/src/bsp/mcu/all/subdir.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-# Add inputs and outputs from these tool invocations to the build variables 
+# Add inputs and outputs from these tool invocations to the build variables
 C_SRCS += \
 ../ra/fsp/src/bsp/mcu/all/bsp_clocks.c \
 ../ra/fsp/src/bsp/mcu/all/bsp_common.c \
@@ -16,7 +16,7 @@ C_SRCS += \
 ../ra/fsp/src/bsp/mcu/all/bsp_rom_registers.c \
 ../ra/fsp/src/bsp/mcu/all/bsp_sbrk.c \
 ../ra/fsp/src/bsp/mcu/all/bsp_sdram.c \
-../ra/fsp/src/bsp/mcu/all/bsp_security.c 
+../ra/fsp/src/bsp/mcu/all/bsp_security.c
 
 C_DEPS += \
 ./ra/fsp/src/bsp/mcu/all/bsp_clocks.d \
@@ -31,7 +31,7 @@ C_DEPS += \
 ./ra/fsp/src/bsp/mcu/all/bsp_rom_registers.d \
 ./ra/fsp/src/bsp/mcu/all/bsp_sbrk.d \
 ./ra/fsp/src/bsp/mcu/all/bsp_sdram.d \
-./ra/fsp/src/bsp/mcu/all/bsp_security.d 
+./ra/fsp/src/bsp/mcu/all/bsp_security.d
 
 OBJS += \
 ./ra/fsp/src/bsp/mcu/all/bsp_clocks.o \
@@ -46,17 +46,17 @@ OBJS += \
 ./ra/fsp/src/bsp/mcu/all/bsp_rom_registers.o \
 ./ra/fsp/src/bsp/mcu/all/bsp_sbrk.o \
 ./ra/fsp/src/bsp/mcu/all/bsp_sdram.o \
-./ra/fsp/src/bsp/mcu/all/bsp_security.o 
+./ra/fsp/src/bsp/mcu/all/bsp_security.o
 
 SREC += \
-microros_testbench.srec 
+microros_testbench.srec
 
 MAP += \
-microros_testbench.map 
+microros_testbench.map
 
 
 # Each subdirectory must supply rules for building sources it contributes
 ra/fsp/src/bsp/mcu/all/%.o: ../ra/fsp/src/bsp/mcu/all/%.c
-	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/src" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/micro_ros_renesas2estudio_component/libmicroros/include" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/fsp/inc" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/fsp/inc/api" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/fsp/inc/instances" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/fsp/src/rm_freertos_port" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra_gen" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra_cfg/fsp_cfg/bsp" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra_cfg/fsp_cfg" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra_cfg/aws" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/fsp/src/rm_wifi_onchip_silex" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/aws/FreeRTOS/FreeRTOS/Source/include" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/fsp/src/rm_aws_sockets_wrapper_wifi_silex" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/aws/FreeRTOS/FreeRTOS-Plus/Source/Utilities/logging" -I"/home/pgarrido/dev/micro-ROS/micro_ros_renesas_testbench/boards/EK_RA6M5/e2studio_project_wifi/ra/arm/CMSIS_6/CMSIS/Core/Include" -I"." -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	$(file > $@.in,-mcpu=cortex-m33 -mthumb -mfloat-abi=hard -mfpu=fpv5-sp-d16 -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal -g -D_RENESAS_RA_ -D_RA_CORE=CM33 -D_RA_ORDINAL=1 -I"../src" -I"../micro_ros_renesas2estudio_component/libmicroros/include" -I"../ra/fsp/inc" -I"../ra/fsp/inc/api" -I"../ra/fsp/inc/instances" -I"../ra/fsp/src/rm_freertos_port" -I"../ra_gen" -I"../ra_cfg/fsp_cfg/bsp" -I"../ra_cfg/fsp_cfg" -I"../ra_cfg/aws" -I"../ra/fsp/src/rm_wifi_onchip_silex" -I"../ra/aws/FreeRTOS/FreeRTOS/Source/include" -I"../ra/fsp/src/rm_aws_sockets_wrapper_wifi_silex" -I"../ra/aws/FreeRTOS/FreeRTOS-Plus/Source/Utilities/logging" -I"../ra/arm/CMSIS_6/CMSIS/Core/Include" -I"." -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
 	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
 
